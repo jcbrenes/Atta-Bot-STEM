7f44dac5792763484fa0507cb2719877e3c73118
Corne1401 <47864642+danielcornejo14@users.noreply.github.com>
2024-07-23 09:31:31 -0600

    chore: bot simulation folder structure

---
+++ App/AttabotAndroidIOS/lib/features/components/bot_simulation.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BotSimulation extends StatelessWidget {
+  final String botImagePath;
+  const BotSimulation({super.key, required this.botImagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ App/AttabotAndroidIOS/lib/features/components/plane_simulation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlaneSimulation extends StatefulWidget {
+  const PlaneSimulation({super.key});
+
+  @override
+  State<PlaneSimulation> createState() => _PlaneSimulationState();
+}
+
+class _PlaneSimulationState extends State<PlaneSimulation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
